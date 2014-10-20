d3400655e8b8a71266ff4c7c218418afde13e29d
Harry Lew <harrylew25@gmail.com>
2014-10-20 17:08:25 -0400

    myAnswer activity is completed
    myAnswer activity is completed with dummy data and ready to apply
    on-click function

--- app/src/main/java/com/android/mobileapp/MyAnswerActivity.java
+++ app/src/main/java/com/android/mobileapp/MyAnswerActivity.java
@@ -1,7 +1,7 @@
 package com.android.mobileapp;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -9,8 +9,13 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Build;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 
 public class MyAnswerActivity extends ActionBarActivity {
@@ -51,6 +56,8 @@ public class MyAnswerActivity extends ActionBarActivity {
      */
     public static class PlaceholderFragment extends Fragment {
 
+        private ArrayAdapter<String> mAnswerAdapter;
+
         public PlaceholderFragment() {
         }
 
@@ -58,6 +65,32 @@ public class MyAnswerActivity extends ActionBarActivity {
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                 Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_my_answer, container, false);
+
+            String[] data = {
+                    "Turn left and go right",
+                    "Nope,it's not free",
+                    "What kind of help?",
+                    "How much ?",
+                    "Nice"
+            };
+
+            List<String> answer = new ArrayList<String>(Arrays.asList(data));
+            mAnswerAdapter = new ArrayAdapter<String>(
+                    getActivity(),
+                    R.layout.list_item_answer,
+                    R.id.list_item_answer_textview,
+                    answer);
+            ListView listView = (ListView) rootView.findViewById(R.id.listview_answer);
+            listView.setAdapter(mAnswerAdapter);
+            listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                    String question = mAnswerAdapter.getItem(position);
+                    Intent intent = new Intent()
+                            .putExtra(Intent.EXTRA_TEXT,question);
+                    startActivity(intent);
+                }
+            });
             return rootView;
         }
     }
