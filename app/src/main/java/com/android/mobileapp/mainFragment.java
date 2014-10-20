f634e88f45eef653fd35470b7c37b443869581a7
Harry Lew <harrylew25@gmail.com>
2014-10-20 02:22:04 -0400

    Nothing changes

--- app/src/main/java/com/android/mobileapp/mainFragment.java
+++ app/src/main/java/com/android/mobileapp/mainFragment.java
@@ -1,8 +1,7 @@
 package com.android.mobileapp;
 
-import android.app.Activity;
+
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
