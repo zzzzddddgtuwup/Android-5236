c94320a0de9b672dca6e3ca9ac16096a32f86b42
Jerry <tjy9206@gmail.com>
2014-11-09 10:47:02 -0500

    Adding location manager

--- app/src/main/java/com/android/mobileapp/MapActivity.java
+++ app/src/main/java/com/android/mobileapp/MapActivity.java
@@ -1,5 +1,9 @@
 package com.android.mobileapp;
 
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
@@ -15,6 +19,7 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.Polygon;
 import com.google.android.gms.maps.model.PolygonOptions;
@@ -35,15 +40,52 @@ public class MapActivity extends ActionBarActivity {
 
     }
 
+    private final LocationListener mLocationListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(Location location) {
+
+        }
+
+        @Override
+        public void onStatusChanged(String s, int i, Bundle bundle) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String s) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String s) {
+
+        }
+    };
+
+    //public static
+
     @Override
     protected  void onStart(){
         super.onStart();
+        LocationManager mLocationManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+        mLocationManager.requestLocationUpdates(mLocationManager.GPS_PROVIDER, 1000L, 500.0f, mLocationListener);
+
         mMap = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
         mMap.setMyLocationEnabled(true);
         mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
-        mMap.moveCamera( CameraUpdateFactory.newLatLngZoom(new LatLng(40, -83.02), 15.0f) );
-        mMap.addMarker(new MarkerOptions().position(new LatLng(40, -83.02))
+        Location currLocation = mLocationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);;
+
+        double currLat = currLocation.getLatitude();
+        double currLng = currLocation.getLongitude();
+        mMap.moveCamera( CameraUpdateFactory.newLatLngZoom(new LatLng(currLat, currLng), 15.0f) );
+        Marker mForum = mMap.addMarker(new MarkerOptions().position(new LatLng(currLat, currLng))
                 .title("Dreese Lab").draggable(true));
+
+        /*
+        mMap.moveCamera( CameraUpdateFactory.newLatLngZoom(new LatLng(40, -83.02), 15.0f) );
+        Marker mForum = mMap.addMarker(new MarkerOptions().position(new LatLng(40, -83.02))
+                .title("Dreese Lab").draggable(true));
+        */
         PolygonOptions rectOptions = new PolygonOptions().add(
                 new LatLng(39.99, -83.01),
                 new LatLng(40.01, -83.01),
