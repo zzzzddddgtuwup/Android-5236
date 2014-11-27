e541e79c1a8d839c0816841b905fda6575774fa5
zzzzddddgtuwup <873087751@qq.com>
2014-11-27 10:59:52 -0500

    login landscape

--- app/src/main/java/com/android/mobileapp/ForumActivity.java
+++ app/src/main/java/com/android/mobileapp/ForumActivity.java
@@ -47,6 +47,8 @@ public class ForumActivity extends ActionBarActivity {
         return true;
     }
 
+
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
