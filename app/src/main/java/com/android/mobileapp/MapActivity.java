cf872086e32425c2f339c48938c097fd5c6abd94
Jerry <tjy9206@gmail.com>
2014-12-01 09:24:48 -0500

    Fix bug for caldwell

--- app/src/main/java/com/android/mobileapp/MapActivity.java
+++ app/src/main/java/com/android/mobileapp/MapActivity.java
@@ -257,7 +257,7 @@ public class MapActivity extends ActionBarActivity {
             return 9;
         }//Ohio stadium
 
-        if (isInArea(lat, lng, 40.001692, 40.002780, -83.015441, -83.001692)) {
+        if (isInArea(lat, lng, 40.001692, 40.002780, -83.015441, -83.014540)) {
             return 10;
         }//Caldwell Laboratory
 
